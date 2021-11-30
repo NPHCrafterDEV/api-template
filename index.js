fba734b6830c7425fd9b048552b500249c844334
NPHCrafterDEV <75496294+NPHCrafterDEV@users.noreply.github.com>
2021-11-30 20:39:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// express is used to build the api
+const express = require('express');
 
+const app = express();
+const port = 3000;
+
+
+app.listen(port, () => console.log('Endpoint started!'))
+
+
+
+app.get('/randomarray', async (req, res) => {
+
+const letters = ["a", "b", "c", "d", "e", "f"]
+const randomletter = letters[Math.floor(Math.random() * letters.length)]; //get a random letter
+
+let list = [];
+list.push({
+randomletter: randomarray
+      
+});
+res.send(list)
+});
